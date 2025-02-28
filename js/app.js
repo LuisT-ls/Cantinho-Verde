cbc6d44c3e25d7fa5448a60fb2a4016e874d8926
LuisT-ls <luishg213@outlook.com>
2025-02-28 12:20:26 -0300

    estrutura principal do projeto

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Importações de módulos (exemplo)
+import { saudacao } from './modules/exemplo.js';
+
+// Código JavaScript principal
+console.log("Aplicação iniciada!");
+
+// Exemplo de uso de função importada
+saudacao();
